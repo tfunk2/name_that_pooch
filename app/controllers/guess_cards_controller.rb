6ad3a73b7e4b215bc0abaa06237c51c76feb8771
tfunk2 <tyler.j.funk2@gmail.com>
2021-01-18 03:09:47 -0700

    Finished demonstration

--- app/controllers/guess_cards_controller.rb
+++ app/controllers/guess_cards_controller.rb
@@ -1,2 +1,23 @@
 class GuessCardsController < ApplicationController
+    def index
+        @guess_cards = GuessCard.all
+        render json: @guess_cards
+    end
+
+    def create
+        @new_guess_card = GuessCard.new(
+            name: params[:name],
+            email: params[:email],
+            breed_one: params[:breed_one],
+            breed_two: params[:breed_two],
+            breed_three: params[:breed_three],
+            breed_four: params[:breed_four],
+            breed_five: params[:breed_five]
+        ) 
+        if @new_guess_card.save
+            render json: @new_guess_card
+        else
+            render json: @new_guess_card.errors.messages
+        end
+    end
 end
